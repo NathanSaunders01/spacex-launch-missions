222cbb5fc88d37350f20631d8f75d24986a0db60
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2018-12-28 20:50:56 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+// const PORT = process.env.PORT || 8080
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
